81c1ca86e252d5a3f7caf065e27fd6df62593a7e
Walfie <walfington@gmail.com>
2017-03-18 23:39:42 -0400

    Add separate `dist` command for optimized builds
    When developing, we don't need minification

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,37 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 
+var isProd = process.env.NODE_ENV === 'production';
+
 var paths = {
   dist: path.resolve('./dist'),
   entry: path.resolve('./src/js/index.js'),
   template: path.resolve('./src/index.html'),
   elmMake: path.resolve('./node_modules/.bin/elm-make')
 }
+
+var nameTemplate = 'assets/[name]' + (isProd ? '[chunkhash:8]' : '');
+
+var plugins = [
+  new ExtractTextPlugin(nameTemplate + '.css'),
+  new HtmlWebpackPlugin({
+    inject: true,
+    template: paths.template,
+    minify: {
+      minifyJS: isProd
+    }
+  }),
+  new CopyWebpackPlugin([
+    { from: 'src/static', to: path.resolve(paths.dist, 'assets') }
+  ])
+].concat(isProd ? [
+  new webpack.optimize.UglifyJsPlugin({}),
+  new CleanWebpackPlugin(['dist'], {
+    root: __dirname,
+    verbose: true,
+    dry: false
+  })
+]: []);
 
 module.exports = {
   entry: paths.entry,
@@ -21,7 +46,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          use: 'css-loader?minimize=true'
+          use: 'css-loader?minimize=' + isProd
         })
       },
       {
@@ -37,27 +62,16 @@ module.exports = {
     ]
   },
   output: {
-    filename: 'assets/[name].[chunkhash:8].js',
+    filename: nameTemplate + '.js',
     path: paths.dist
   },
-  plugins: [
-    new ExtractTextPlugin("assets/[name].[chunkhash:8].css"),
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: paths.template,
-      minify: {
-        minifyJS: true
-      }
-    }),
-    new CopyWebpackPlugin([
-      { from: 'src/static', to: path.resolve(paths.dist, 'assets') }
-    ]),
-    new webpack.optimize.UglifyJsPlugin({}),
-    new CleanWebpackPlugin(['dist'], {
-      root: __dirname,
-      verbose: true,
-      dry: false
-    })
-  ]
+  plugins: plugins,
+  stats: isProd ? 'normal' : {
+    assets: false,
+    children: false,
+    chunks: false,
+    chunkModules: false,
+    chunkOrigins: false
+  }
 };
 
